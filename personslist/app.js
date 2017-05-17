ddece4028c42a08b66c5733772bcb26dde658481
I045112 <I045112@INLN50939860A.apj.global.corp.sap>
2017-05-17 07:07:05 +0530

    Re-factored code

--- personslist/app.js
+++ personslist/app.js
@@ -5,10 +5,6 @@ var app = express();
 
 app.use('/ui5', express.static(path.join(__dirname, 'webapp')));
 
-app.get('/',function(req,res){
-	res.send("Hello World!");
-});
-
 app.listen(process.env.PORT || 3000, function () {
-	console.log('Example app listening on port 3000!');
+	console.log('Personlist app is listening on port 3000!');
 });
